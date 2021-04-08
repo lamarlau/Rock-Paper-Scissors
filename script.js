80a621ec18a0600f006352b7033618c02fb3adad
lamarlau <lauren.lamar55@outlook.com>
2021-04-08 14:03:57 -0500

    Added functions and descriptions of the game

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Hello World!")
+//can be completely run inside the console
+var selection = ["Rock, Paper, Scissors"]//for computer selection
+
+//computer play is to randomly choose rock paper or scissors
+function computerPlay {
+    
+}
+
+//playround takes parameters playerselection and computerselection
+//returns string at end like "Paper beats rock"
+//player selection must be case insensitive
+//return results of function call not console.log them
+function playRound {
+
+}
+
+//use previous function inside this one to play a 5 round game
+//keeps score and reports winner or loser at the end
+function game {
+
+}
+
+console.log()//display the results of the round and winner to the user
+prompt()//to get input from user
