d4ab86e756787d7aa21c6f6e67dcf88d5d3df171
lamarlau <lauren.lamar55@outlook.com>
2021-04-10 12:06:18 -0500

    Added random index and parentheses

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 console.log("Hello World!")
 //can be completely run inside the console
 var selection = ["Rock, Paper, Scissors"]//for computer selection
+const randIndex = Math.floor(Math.random() * selection.length);
 
 //computer play is to randomly choose rock paper or scissors
-function computerPlay {
-    
+function computerPlay() {
+    console.log(selection[randIndex]);
 }
 
 //playround takes parameters playerselection and computerselection
 //returns string at end like "Paper beats rock"
 //player selection must be case insensitive
 //return results of function call not console.log them
-function playRound {
+function playRound() {
 
 }
 
 //use previous function inside this one to play a 5 round game
 //keeps score and reports winner or loser at the end
-function game {
+function game() {
 
 }
 
