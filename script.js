af398cfeaee7243e515d51fac0bb15a8860bdf26
lamarlau <lauren.lamar55@outlook.com>
2021-04-11 10:33:30 -0500

    Added a regex for making the playerselection case insensitive.

--- script.js
+++ script.js
@@ -12,9 +12,19 @@ function computerPlay() {
 //returns string at end like "Paper beats rock"
 //player selection must be case insensitive
 //return results of function call not console.log them
-function playRound() {
-
+function playRound(playerselection, computerselection) {
+choice = prompt("Rock, paper or scissors?");
+let choice = new RegExp(/rock/i, /paper/i, /scissors/i);
+playerselection = choice;
+computerselection = computerPlay;
+if (computerselection = playerselection) {
+    roundResults = tie;
+    tie = "Tie, you both chose ${choice}"
+    return roundResults;
 }
+return roundResults;
+}
+/*I need to somehow connect choice to playerselection. I need to make it so that the if statement can recognize if playerselection and computerselection are equal. I need to add logic about how one choice is greater than or less than another choice. I need to finish the if/else logic for when computerselection wins, and when playerselection wins.*/
 
 //use previous function inside this one to play a 5 round game
 //keeps score and reports winner or loser at the end
