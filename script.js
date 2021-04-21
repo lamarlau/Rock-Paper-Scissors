3ec715c8b8aca41f54146e83c2ce3a58297d3b40
lamarlau <lauren.lamar55@outlook.com>
2021-04-20 19:53:29 -0500

    Update script.js
    made playround loop into just calling it 5 times. Scoring system doesnt work because the result is always noone won.

--- script.js
+++ script.js
@@ -1,47 +1,53 @@
 //initializing variables, creating an array for selection, a random index constant, and a results string to call after each round of the game
-let computerScore = 0;
-let playerScore = 0;
+var computerScore = 0;
+var playerScore = 0;
 var selection = ["rock", "paper", "scissors"];
 const randIndex = Math.floor(Math.random() * selection.length);
-let results = "Computer score: " + computerScore + "; Player score: " + playerScore;
+var results = "Computer score: " + computerScore + "; Player score: " + playerScore;
 var computerResult;
+var choice;
+var round = 0;
 
 //function for computer to randomly choose from variable selection
 function computerPlay() {
     computerResult = selection[randIndex];
     console.log(computerResult);
-    return computerResult;
 }
 
-/* play round takes case insensitive input and compares your selection with the computers selection. Adds score and returns  */
+function playerPlay() {
+    choice = prompt("Rock, paper or scissors?");
+    choice.toLowerCase;
+}
+/* play round takes case insensitive input and compares your selection with the computers selection. Adds score and returns string */
 function playRound(playerselection, computerselection) {
-    var choice = prompt("Rock, paper or scissors?");
-    choice.toLowerCase();
+    playerPlay();
     computerPlay();
-    if (choice = playerselection) {
+    choice = playerselection;
+    computerResult = computerselection;
+    if (playerselection = computerResult) {
         return "Tie, you both chose ${choice}";
     }
-    else if (choice == selection[0] && computerResult == selection[1]) {
+    else if (playerselection == selection[0] && computerselection == selection[1]) {
         computerScore++;
         return "Paper beats rock";
     }
-    else if (choice == selection[1] && computerResult == selection[2]) {
+    else if (playerselection == selection[1] && computerselection == selection[2]) {
         computerScore++;
         return "Scissors beats paper";
     }
-    else if (choice == selection[2] && computerResult == selection[1]) {
+    else if (playerselection == selection[2] && computerselection == selection[1]) {
         playerScore++;
         return "Scissors beats paper";
     }
-    else if (choice == selection[1] && computerResult == selection[0]) {
+    else if (playerselection == selection[1] && computerselection == selection[0]) {
         playerScore++;
         return "Paper beats rock";
     }
-    else if (choice == selection[0] && computerResult == selection[2]) {
+    else if (playerselection == selection[0] && computerselection == selection[2]) {
         playerScore++;
         return "Rock beats scissors";
     }
-    else if (choice == selection[2] && computerResult == selection[0]) {
+    else if (playerselection == selection[2] && computerselection == selection[0]) {
         computerScore++;
         return "Rock beats scissors";
     }
@@ -50,19 +56,21 @@ function playRound(playerselection, computerselection) {
 //use previous function inside this one to play a 5 round game
 //keeps score and reports winner or loser at the end
 function game() {
-    round = 0;
-    while (round <= 5) {
-        round++;
         playRound();
+        playRound();
+        playRound();
+        playRound();
+        playRound();
+    if (computerScore > playerScore) {
+        console.log("Computer wins");
         return results;
     }
-    if (computerScore > playerScore) {
-        console.log("Computer wins")
+    else if (computerScore < playerScore) {
+        console.log("You won!");
         return results;
     }
     else {
-        console.log("You won!")
-        return results;
+        console.log("Noone won");
     }
 };
 
