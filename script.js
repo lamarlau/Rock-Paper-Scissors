f22a7622939445a2ac5374d1a5db33cc48196b15
lamarlau <lauren.lamar55@outlook.com>
2021-04-23 10:39:15 -0500

    Update script.js
    Added an else statement in case there is a tie

--- script.js
+++ script.js
@@ -69,6 +69,9 @@ function game() {
     else if (computerScore < playerScore) {
         console.log("You won!");
     }
+    else {
+        console.log("You tied with the computer");
+    }
 }
 
 
